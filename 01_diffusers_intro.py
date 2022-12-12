836486d6ede4cdba6135e92cbf7ad3131a52060c
Gary Hutson <gary.hutson.uk@hotmail.co.uk>
2022-12-12 17:00:29 +0000

    Added plots and code

--- 01_diffusers_intro.py
+++ 01_diffusers_intro.py
@@ -65,3 +65,155 @@ from torchvision import transforms
 dataset = load_dataset("huggan/smithsonian_butterflies_subset", split="train")
 image_size = 32 
 batch_size = 64
+
+import torchvision
+from datasets import load_dataset
+from torchvision import transforms
+
+dataset = load_dataset("huggan/smithsonian_butterflies_subset", split="train")
+
+# Or load images from a local folder
+# dataset = load_dataset("imagefolder", data_dir="path/to/folder")
+
+# We'll train on 32-pixel square images, but you can try larger sizes too
+image_size = 32
+# You can lower your batch size if you're running out of GPU memory
+batch_size = 64
+
+# Define data augmentations
+preprocess = transforms.Compose(
+    [
+        transforms.Resize((image_size, image_size)),  # Resize
+        transforms.RandomHorizontalFlip(),  # Randomly flip (data augmentation)
+        transforms.ToTensor(),  # Convert to tensor (0, 1)
+        transforms.Normalize([0.5], [0.5]),  # Map to (-1, 1)
+    ]
+)
+
+
+def transform(examples):
+    images = [preprocess(image.convert("RGB")) for image in examples["image"]]
+    return {"images": images}
+
+
+dataset.set_transform(transform)
+
+# Create a dataloader from the dataset to serve up the transformed images in batches
+train_dataloader = torch.utils.data.DataLoader(
+    dataset, batch_size=batch_size, shuffle=True
+)
+
+def transform(examples):
+    images = [preprocess(image.convert("RGB")) for image in examples["image"]]
+    return {"images": images}
+
+
+dataset.set_transform(transform)
+
+# Create a dataloader from the dataset to serve up the transformed images in batches
+train_dataloader = torch.utils.data.DataLoader(
+    dataset, batch_size=batch_size, shuffle=True
+)
+
+# Create an iterator to process the images
+xb = next(iter(train_dataloader))['images'].to(device)[:8]
+print("X shape:", xb.shape)
+show_images(xb).resize((8 * 64, 64), resample=Image.NEAREST)
+
+from diffusers import DDPMScheduler
+noise_scheduler = DDPMScheduler(num_train_timesteps=1000)
+
+# Create a scheduler and corrupt each image with a small amount of noise per time step
+plt.plot(noise_scheduler.alphas_cumprod.cpu() ** 0.5, label=r"${\sqrt{\bar{\alpha}_t}}$")
+plt.plot((1 - noise_scheduler.alphas_cumprod.cpu()) ** 0.5, label=r"$\sqrt{(1 - \bar{\alpha}_t)}$")
+plt.legend(fontsize="x-large")
+plt.savefig('images/noising_plot.jpeg')
+
+# Show noising steps across grid
+timesteps = torch.linspace(0, 999, 8).long().to(device)
+noise = torch.randn_like(xb)
+noisy_xb = noise_scheduler.add_noise(xb, noise, timesteps)
+print("Noisy X shape", noisy_xb.shape)
+images = show_images(noisy_xb).resize((8 * 64, 64), resample=Image.NEAREST)
+images.save('images/noised_images.jpg')
+
+# Get unnet model to down and upsample
+from diffusers import UNet2DModel
+
+# Create a model
+model = UNet2DModel(
+    sample_size=image_size,  # the target image resolution
+    in_channels=3,  # the number of input channels, 3 for RGB images
+    out_channels=3,  # the number of output channels
+    layers_per_block=2,  # how many ResNet layers to use per UNet block
+    block_out_channels=(64, 128, 128, 256),  # More channels -> more parameters
+    down_block_types=(
+        "DownBlock2D",  # a regular ResNet downsampling block
+        "DownBlock2D",
+        "AttnDownBlock2D",  # a ResNet downsampling block with spatial self-attention
+        "AttnDownBlock2D",
+    ),
+    up_block_types=(
+        "AttnUpBlock2D",
+        "AttnUpBlock2D",  # a ResNet upsampling block with spatial self-attention
+        "UpBlock2D",
+        "UpBlock2D",  # a regular ResNet upsampling block
+    ),
+)
+model.to(device)
+
+with torch.no_grad():
+    model_prediction = model(noisy_xb, timesteps).sample
+model_prediction.shape
+
+# Create a training loop
+# Set the noise scheduler
+noise_scheduler = DDPMScheduler(
+    num_train_timesteps=1000, beta_schedule="squaredcos_cap_v2"
+)
+
+# Training loop
+optimizer = torch.optim.AdamW(model.parameters(), lr=4e-4)
+
+losses = []
+
+for epoch in range(30):
+    for step, batch in enumerate(train_dataloader):
+        clean_images = batch["images"].to(device)
+        # Sample noise to add to the images
+        noise = torch.randn(clean_images.shape).to(clean_images.device)
+        bs = clean_images.shape[0]
+
+        # Sample a random timestep for each image
+        timesteps = torch.randint(
+            0, noise_scheduler.num_train_timesteps, (bs,), device=clean_images.device
+        ).long()
+
+        # Add noise to the clean images according to the noise magnitude at each timestep
+        noisy_images = noise_scheduler.add_noise(clean_images, noise, timesteps)
+
+        # Get the model prediction
+        noise_pred = model(noisy_images, timesteps, return_dict=False)[0]
+
+        # Calculate the loss
+        loss = F.mse_loss(noise_pred, noise)
+        loss.backward(loss)
+        losses.append(loss.item())
+
+        # Update the model parameters with the optimizer
+        optimizer.step()
+        optimizer.zero_grad()
+
+    if (epoch + 1) % 5 == 0:
+        loss_last_epoch = sum(losses[-len(train_dataloader) :]) / len(train_dataloader)
+        print(f"Epoch:{epoch+1}, loss: {loss_last_epoch}")
+
+
+# Visualise the training progress
+fig, axs = plt.subplots(1, 2, figsize=(12, 4))
+axs[0].plot(losses)
+axs[1].plot(np.log(losses))
+plt.savefig('images/training_progress.jpg')
+
+# Uncomment to instead load the model I trained earlier:
+# model = butterfly_pipeline.unet
